867e9472c2b41a22041072d328228c0cdca1838d
Дарья Соловьева <dariasoloveva@Darias-New-MacBook-Pro.local>
2020-12-18 15:30:42 +0400

    update option 2

--- option-2/js/load-shop.js
+++ option-2/js/load-shop.js
@@ -2,28 +2,30 @@ const itemContent = `<a href="./product.html">
 <img
   class="card-img-top"
   alt="PATRÓN SILVER Tequila"
-  src="./images/bacardi-white.webp"
+  src="./images/tequila.png"
 />
 </a>
-<div class="card-body">
+<div class="card-body text-center">
 <a href="./product.html">PATRÓN SILVER Tequila</a>
-<select class="avc-o-input avc-o-input--select">
+<select class="w-100 my-3 p-2">
   <option value="375 mL">375 mL</option>
   <option value="750 mL">750 mL</option>
   <option value="1.75 L">1.75 L</option>
 </select>
 <p class="">Starting at $16.99</p>
-<a href="./product.html" class=""><span>Buy Now</span></a>
+<a href="./product.html" class=""><button class="buy btn rounded-0 w-100">Buy Now</button></a>
 </div>`;
 
 const item = `<!-- card -->
-<div class="card mb-5" style="width: 18rem">
+<div class="card mb-5 border-0" style="width: 18rem">
   ${itemContent}
 </div>
 <!-- card end -->`;
 
 const initContent = `
-<div id="products" class="d-flex justify-content-around flex-wrap p-5 m-5 bg-light">
+<div id="products" class="products d-flex justify-content-around flex-wrap p-5 m-5">
+  ${item}
+  ${item}
   ${item}
   ${item}
   ${item}
@@ -43,14 +45,14 @@ const createItem = () => {
   return el;
 };
 
-document.querySelector("button#add").addEventListener("click", () => {
-  document.querySelector("div#products").appendChild(createItem());
-});
+// document.querySelector("button#add").addEventListener("click", () => {
+//   document.querySelector("div#products").appendChild(createItem());
+// });
 
-document.querySelector("button#remove").addEventListener("click", () => {
-  document.querySelector("div.card").remove();
-});
+// document.querySelector("button#remove").addEventListener("click", () => {
+//   document.querySelector("div.card").remove();
+// });
 
 setTimeout(() => {
   container.innerHTML = initContent;
-}, 2000);
+}, 0);
